8f8034173d9c75587fce646ad2a05953e50e6203
s23575 <s23575@pjwstk.edu.pl>
2022-06-12 23:06:45 +0200

    Rates_v_1_0

---
+++ rates/src/main/java/pl/edu/pjatk/jaz/nbp/rates/NbpAverageExchangeRatesApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.jaz.nbp.rates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbpAverageExchangeRatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbpAverageExchangeRatesApplication.class, args);
+	}
+
+}
---
+++ rates/src/test/java/pl/edu/pjatk/jaz/nbp/rates/NbpAverageExchangeRatesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.jaz.nbp.rates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbpAverageExchangeRatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
