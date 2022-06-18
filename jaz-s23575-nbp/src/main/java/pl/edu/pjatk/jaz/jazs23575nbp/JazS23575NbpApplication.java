891245648c3691668d94b3f087d18e08e41ef755
s23575 <s23575@pjwstk.edu.pl>
2022-06-18 11:52:22 +0200

    jaz-s23575-nbp_v1

---
+++ jaz-s23575-nbp/src/main/java/pl/edu/pjatk/jaz/jazs23575nbp/JazS23575NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.jaz.jazs23575nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23575NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23575NbpApplication.class, args);
+	}
+
+}
---
+++ jaz-s23575-nbp/src/test/java/pl/edu/pjatk/jaz/jazs23575nbp/JazS23575NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pjatk.jaz.jazs23575nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23575NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
